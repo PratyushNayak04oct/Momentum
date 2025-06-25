48ff3fd1c02bb9f3043736a608cfb6bada4f60f2
Pratyush <pratyush041002@gmail.com>
2025-06-25 07:39:36 +0530

    Changes config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    devIndicators: false,
+    images: {
+        domains: ['images.pexels.com'],
+    },
+};
 
 export default nextConfig;
